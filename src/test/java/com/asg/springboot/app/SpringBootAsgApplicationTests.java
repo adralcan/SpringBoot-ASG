46424ec4e45661a811489c822fbc4e3c495a6987
adralcan <adrian97ad@gmail.com>
2022-08-20 12:40:00 +0200

    Configuracion inicial del nuevo proyecto

---
+++ src/test/java/com/asg/springboot/app/SpringBootAsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asg.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAsgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
